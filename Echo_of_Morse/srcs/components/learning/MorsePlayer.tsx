a42a0f5df229a6a80eade0675499446df5b06cf9
mlaurent <mlaurent@f6r3s16.paris.42.school>
2026-04-28 13:49:36 +0200

    feat : websocket draft

--- Echo_of_Morse/srcs/components/learning/MorsePlayer.tsx
+++ Echo_of_Morse/srcs/components/learning/MorsePlayer.tsx
@@ -63,3 +63,54 @@ export default function MorsePlayer() {
     </section>
   );
 }
+
+
+
+
+// "use client";
+
+// import { useEffect, useState } from "react";
+// import { io, Socket } from "socket.io-client";
+// import { encode } from "@/lib/morse";
+// import { playMorse } from "@/lib/audio";
+
+// let socket: Socket;
+
+//   async function handlePlay(text: string) 
+//   {
+//     const morse = encode(text);
+//     await playMorse(morse);
+//     socket.emit("send-morse", morse);
+//   }
+
+// export default function MorsePlayer() 
+// {
+//   const [text, setText] = useState("");
+
+//   useEffect(() => {
+//     socket = io("http://localhost:3001");
+
+//     const handler = async (morse: string) => {
+//       console.log("Received:", morse);
+//       await playMorse(morse);
+//     };
+
+//     socket.on("receive-morse", handler);
+
+//     return () => {
+//       socket.off("receive-morse", handler);
+//       socket.disconnect();
+//     };
+//   }, []);
+
+//   return (
+//     <div>
+//       <input
+//         value={text}
+//         onChange={(e) => setText(e.target.value)}
+//         placeholder="Enter text"
+//       />
+//       <button onClick={() => handlePlay(text)}>Play</button>
+//     </div>
+//   );
+// }
---
+++ Echo_of_Morse/ws-server/server.js
@@ -0,0 +1,31 @@
+// import { Server } from "socket.io";
+
+// const io = new Server(3001, { cors: { origin: "*" }});
+
+// io.on("connection", (socket) => {
+    
+//   console.log("User connected");
+//   socket.on("send-morse", (data: string) => { socket.broadcast.emit("receive-morse", data); });
+//   socket.on("disconnect", () => { console.log("User disconnected"); });
+
+// });
+
+
+
+const { Server } = require("socket.io");
+
+const io = new Server(3001, {
+  cors: { origin: "*" }
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  socket.on("send-morse", (data) => {
+    socket.broadcast.emit("receive-morse", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
