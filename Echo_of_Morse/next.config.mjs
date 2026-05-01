00558d7ae58431d45b2be3cfaa9c25793a2e6b48
Gustavo Daniel Gonzalez <poppingusty.2605@gmail.com>
2026-05-01 12:07:37 +0200

    merge: intégrer le service ws de l'équipe + résoudre le conflit docker-compose

--- Echo_of_Morse/next.config.mjs
+++ Echo_of_Morse/next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  experimental: {
+    instrumentationHook: true, // active instrumentation.js au démarrage
+  },
 };
 
 export default nextConfig;
